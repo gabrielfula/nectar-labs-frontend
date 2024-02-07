38326a4c5e58a5d65aef14b1cbc15e473933da56
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-07 08:36:54 -0300

    feat: add petlist to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Navbar from "./components/Navbar";
+import PetList from "./components/PetList";
+
 export default function App() {
   return (
     <>
-      <div>App</div>
+      <Navbar />
+      <div className="p-14">
+        <PetList />
+      </div>
     </>
   );
 }
