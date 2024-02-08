f574a5f36411818eba7602d07685662ab2bddf23
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-07 21:48:37 -0300

    chore: change field id props

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,18 @@
+import { z } from "zod";
+
 export interface PetsProps {
-  id: string;
+  id: number;
   nome: string;
   idade: number;
   especie: string;
   raca: string;
   adocao: boolean;
 }
+
+export const createClientSchema = z.object({
+  nome: z.string().nonempty("Nome é obrigatório"),
+  especie: z.string().nonempty("Espécie é obrigatório"),
+  raca: z.string().nonempty("Raça é obrigatório"),
+  idade: z.number(),
+  adocao: z.string().nonempty("Escolha o status da adoção."),
+});
